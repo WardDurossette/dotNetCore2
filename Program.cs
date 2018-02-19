09ac7cb9c690bc273539962b2bac3146f2c6fce1
Durossette, Ward <RDurossette@twu.edu>
2018-02-19 13:06:47 -0600

    Merged PR 49: Added support for wiring up user-secrets.
    Added support for wiring up user-secrets.

--- Program.cs
+++ Program.cs
@@ -10,31 +10,32 @@ using Microsoft.Extensions.Logging;
 
 namespace WWWROOT
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-            
-              .UseContentRoot(Directory.GetCurrentDirectory())
-              
-              
-
-              .ConfigureAppConfiguration((builderContext, config) => {
-                IHostingEnvironment env = builderContext.HostingEnvironment;
-
-                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                  .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
-              
-                config.AddEnvironmentVariables();
-
-              })
-              
-              .UseStartup<Startup>()
-              .Build();
+        BuildWebHost(args).Run();
     }
+
+    public static IWebHost BuildWebHost(string[] args) =>
+      WebHost.CreateDefaultBuilder(args)
+        
+      .UseContentRoot(Directory.GetCurrentDirectory())
+          
+
+      .ConfigureAppConfiguration((builderContext, config) => {
+        IHostingEnvironment env = builderContext.HostingEnvironment;
+
+        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+          
+        config.AddEnvironmentVariables();
+        if (env.IsDevelopment())
+        {
+            config.AddUserSecrets<Startup>();
+        }
+      })
+      .UseStartup<Startup>()
+      .Build();
+  }
 }
